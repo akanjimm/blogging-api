efaf12dc07985be46b3ca22e5dcf653d7284477d
Mohammed Akanji <akanjimm@gmail.com>
2022-11-06 21:10:05 +0100

    update controller for updateBlogByID function

--- controllers/blog.controller.js
+++ controllers/blog.controller.js
@@ -16,7 +16,7 @@ function getBlogsByUserID(req, res, next) {
 function addBlog(req, res, next) {
     const user = req.user;
     const reading_time = calcReadingTime(req.body.body);
-    const blog = {...req.body, author: user._id, reading_time};
+    const blog = { ...req.body, author: user._id, reading_time };
 
     if (!blog.title || !blog.body) {
         return next(new Error("Title and body fields are required"))
@@ -35,7 +35,37 @@ function addBlog(req, res, next) {
 }
 
 function updateBlogByID(req, res, next) {
-    console.log("blog updated");
+    const blogId = req.params.id;
+    let blogUpdate = req.body;
+    const loggedInUser = req.user;
+
+    // Prevent manual updating of author, read_count and reading_time
+    if (blogUpdate.author || blogUpdate.read_count || blogUpdate.reading_time) {
+        return next(new Error("Sorry, you can't update author, read count and reading time"));
+    }
+
+    blogModel.findOne({ _id: blogId })
+        .then(async (blog) => {
+            // check if blog owner is the current loggedin user
+            if (loggedInUser._id !== blog.author.toString()) {
+                return next(new Error("Sorry, you are not the blog owner. You can't update blog."))
+            }
+
+            // update blog
+            try {
+                blogUpdate.updatedAt = new Date();
+                const updatedBlog = await blogModel.findByIdAndUpdate(blogId, blogUpdate, { new: true });
+                res.status(200).send({
+                    message: "Blog post successfully updated",
+                    data: { updatedBlog }
+                });
+            } catch (err) {
+                next(err);
+            }
+        })
+        .catch((err) => {
+            next(err);
+        })
 }
 
 function deleteBlogByID(req, res, next) {
